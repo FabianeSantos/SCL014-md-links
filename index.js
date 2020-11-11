fafcf050de2b2e4df2a39503a63f04fe3197a575
Fabiane Santos <fanutri1@gmail.com>
2020-11-11 11:51:47 -0300

    se actualiza readme

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-module.exports = () => {
-  // ...
+// module.exports = () => {
+//   // ...
+// };
+
+//instalar modulo axios
+const axios = require("axios");
+
+const greet = (name) => "Hello" + name;
+// hacer una petición a una api, que contiene datos
+// me retorna una promesa
+const users = async () => {
+  const res = await axios.get(
+    "https://jsonplaceholder.typicode.com/users?_limit=2"
+  );
+  return res.data;
 };
+users().then((res) => console.log(res));
+
+module.exports = { users, greet };
